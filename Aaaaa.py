4742fe6446296617a2a9daaadd860c29d98517b4
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-09-30 22:19:25 +0200

    Project X in work

--- Aaaaa.py
+++ Aaaaa.py
@@ -136,43 +136,43 @@
 # print(repr(c))
 
 ### метод super() и MRO (Method Resolution Order)
-class A:
-    def show(self):
-        print("Метод show в классе A")
-
-class B(A):
-    def show(self):
-        print("Метод show в классе B")
-        super().show()
-
-class C(A):
-    def show(self):
-        print("Метод show в классе C")
-        super().show()
-
-class D(C, B):
-    pass
-
-# Создаем объект класса D и вызываем его метод show
-obj = D()
-obj.show()
+# class A:
+#     def show(self):
+#         print("Метод show в классе A")
+#
+# class B(A):
+#     def show(self):
+#         print("Метод show в классе B")
+#         super().show()
+#
+# class C(A):
+#     def show(self):
+#         print("Метод show в классе C")
+#         super().show()
+#
+# class D(C, B):
+#     pass
+#
+# # Создаем объект класса D и вызываем его метод show
+# obj = D()
+# obj.show()
 
 ### Mixin Classes
 # Пример класса-миксина, предоставляющего логирование
-class LoggingMixin:
-    def log(self, message):
-        print(f"class [{self.__class__.__name__}] {message}")
-
-# Пример класса, использующего класс-миксин
-class Calculator(LoggingMixin):
-    def add(self, x, y):
-        result = x + y
-        self.log(f"Вычисление: {x} + {y} = {result}")
-        return result
-
-# Создаем объект класса Calculator и используем логирование из класса-миксина
-calc = Calculator()
-calc.add(3, 5)
+# class LoggingMixin:
+#     def log(self, message):
+#         print(f"class [{self.__class__.__name__}] {message}")
+#
+# # Пример класса, использующего класс-миксин
+# class Calculator(LoggingMixin):
+#     def add(self, x, y):
+#         result = x + y
+#         self.log(f"Вычисление: {x} + {y} = {result}")
+#         return result
+#
+# # Создаем объект класса Calculator и используем логирование из класса-миксина
+# calc = Calculator()
+# calc.add(3, 5)
 
 ###
 
